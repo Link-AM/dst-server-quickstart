43bbef42d17fabe39ab2c2f52115d57466733892
Alex Link <alexlink809@gmail.com>
2021-10-25 17:51:09 -0400

    SteamCMD_Location variable added

--- StartDSTServers.bat
+++ StartDSTServers.bat
@@ -1,4 +1,11 @@
-c:\steamcmd\steamcmd.exe +login anonymous +app_update 343050 +quit
-cd /D "c:\steamcmd\steamapps\common\Don't Starve Together Dedicated Server\bin"
+:: Add the installation folder of your SteamCMD here in the quotes
+set SteamCMD_Location="c:\steamcmd"
+
+
+::_______________________________________________________________________________________
+
+
+"%SteamCMD_Location%" & "\steamcmd.exe" +login anonymous +app_update 343050 +quit
+cd /D "%SteamCMD_Location%" & "\steamapps\common\Don't Starve Together Dedicated Server\bin"
 start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Master
 start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Caves
