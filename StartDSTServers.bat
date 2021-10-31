b5f99c750dd7329746a14fdaa64c08b18344e7fb
Alex Link <alexlink809@gmail.com>
2021-10-31 18:29:21 -0400

    variable quickfix

--- StartDSTServers.bat
+++ StartDSTServers.bat
@@ -5,7 +5,8 @@ set SteamCMD_Location="c:\steamcmd"
 ::_______________________________________________________________________________________
 
 
-"%SteamCMD_Location%" & "\steamcmd.exe" +login anonymous +app_update 343050 +quit
-cd /D "%SteamCMD_Location%" & "\steamapps\common\Don't Starve Together Dedicated Server\bin"
+%SteamCMD_Location%"\steamcmd.exe" +login anonymous +app_update 343050 +quit
+cd /D %SteamCMD_Location%"\steamapps\common\Don't Starve Together Dedicated Server\bin"
+pause
 start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Master
 start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Caves
