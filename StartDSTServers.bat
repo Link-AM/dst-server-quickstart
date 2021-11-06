435e2520180ed99eefe90165b0f83dcbc16565ca
Alex Link <alexlink809@gmail.com>
2021-11-06 15:39:43 -0400

    extra comments added.  test folder removed

--- StartDSTServers.bat
+++ StartDSTServers.bat
@@ -32,17 +32,19 @@ set "cavesLua=\Caves\modoverrides.lua"
 set "serverLua=%cmdDir%\mods\dedicated_server_mods_setup.lua"
 set "templateLua=template\dedicated_server_mods_setup.lua"
 
+::	Copy modoverrides.lua from the Master folder
 echo. && echo Copy "%Cluster_Location%%masterLua%" to "%DediServer_Location%%masterLua%"
 copy /b/v/y "%Cluster_Location%%masterLua%" "%DediServer_Location%%masterLua%"
 
+::	Copy modoverrides.lua from the Caves folder
 echo. && echo Copy "%Cluster_Location%%cavesLua%" to "%DediServer_Location%%cavesLua%"
 copy /b/v/y "%Cluster_Location%%cavesLua%" "%DediServer_Location%%cavesLua%"
 
-::	Move dedicated_server_mods_setup.lua from template location to SteamCMD location
+::	Replace dedicated_server_mods_setup.lua with a blank one
 echo. && echo Copy "%cd%\%templateLua%" to "%serverLua%"
 copy /b/v/y "%templateLua%" "%serverLua%"
 
-:: 	Copy the workshop IDs from modoverrides.lua into dedicated_server_mods_setup.lua
+:: 	Import the workshop IDs from modoverrides.lua into dedicated_server_mods_setup.lua
 echo. && echo Import workshop-id's from "%DediServer_Location%%masterLua%" into "%serverLua%"
 setlocal enableDelayedExpansion
 set /a count = 0
@@ -56,9 +58,9 @@ for /f delims^=^] %%a in ('findstr /r "workshop-" "%DediServer_Location%%masterL
 )
 echo 	%count% workshop-id(s) imported.
 
+::	Start the dedicated server
 echo. && echo. && echo Script will now attempt start the server. && echo.
 pause
-
 %SteamCMD_Location%"\steamcmd.exe" +login anonymous +app_update 343050 +quit
 cd /D %binDir%
 start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Master
