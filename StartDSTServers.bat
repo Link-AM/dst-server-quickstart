2ed66cea0ac760a8d7e1d351e11e71e0d02fa53d
Alex Link <alexlink809@gmail.com>
2021-11-09 19:16:27 -0500

    template file removed.  Klei's steps removed from readme

--- StartDSTServers.bat
+++ StartDSTServers.bat
@@ -7,7 +7,7 @@
 
 
 set "SteamCMD_Location=C:\steamcmd"
-set "Cluster_Location=C:\Users\alexl\Documents\Klei\DoNotStarveTogether\94654202\Cluster_2"
+set "Cluster_Location=C:\Users\alexl\Documents\Klei\DoNotStarveTogether\94654202\Cluster_1"
 set "DediServer_Location=C:\Users\alexl\Documents\Klei\DoNotStarveTogether\MyDediServer"
 
 
@@ -27,10 +27,9 @@ echo.
 
 set "cmdDir=%SteamCMD_Location%\steamapps\common\Don't Starve Together Dedicated Server"
 set "binDir=%cmdDir%\bin"
+set "serverLua=%cmdDir%\mods\dedicated_server_mods_setup.lua"
 set "masterLua=\Master\modoverrides.lua"
 set "cavesLua=\Caves\modoverrides.lua"
-set "serverLua=%cmdDir%\mods\dedicated_server_mods_setup.lua"
-set "templateLua=template\dedicated_server_mods_setup.lua"
 
 ::	Copy modoverrides.lua from the Master folder
 echo. && echo Copy "%Cluster_Location%%masterLua%" to "%DediServer_Location%%masterLua%"
@@ -40,9 +39,23 @@ copy /b/v/y "%Cluster_Location%%masterLua%" "%DediServer_Location%%masterLua%"
 echo. && echo Copy "%Cluster_Location%%cavesLua%" to "%DediServer_Location%%cavesLua%"
 copy /b/v/y "%Cluster_Location%%cavesLua%" "%DediServer_Location%%cavesLua%"
 
-::	Replace dedicated_server_mods_setup.lua with a blank one
-echo. && echo Copy "%cd%\%templateLua%" to "%serverLua%"
-copy /b/v/y "%templateLua%" "%serverLua%"
+::	Replace dedicated_server_mods_setup.lua with the original blank one
+echo. && echo Restore "%serverLua%" file to its original state
+(
+echo --There are two functions that will install mods, ServerModSetup and ServerModCollectionSetup. Put the calls to the functions in this file and they will be executed on boot.
+echo.
+echo --ServerModSetup takes a string of a specific mod's Workshop id. It will download and install the mod to your mod directory on boot.
+echo 	--The Workshop id can be found at the end of the url to the mod's Workshop page.
+echo 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=350811795
+echo 	--ServerModSetup^("350811795"^)
+echo.
+echo --ServerModCollectionSetup takes a string of a specific mod's Workshop id. It will download all the mods in the collection and install them to the mod directory on boot.
+echo 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
+echo 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
+echo 	--ServerModCollectionSetup^("379114180"^)
+echo.
+)>"%serverLua%"
+echo 	File restoration complete
 
 :: 	Import the workshop IDs from modoverrides.lua into dedicated_server_mods_setup.lua
 echo. && echo Import workshop-id's from "%DediServer_Location%%masterLua%" into "%serverLua%"
@@ -56,11 +69,12 @@ for /f delims^=^] %%a in ('findstr /r "workshop-" "%DediServer_Location%%masterL
 		echo ServerModSetup("!wsid!"^)>> "%serverLua%"
     ))
 )
-echo 	%count% workshop-id(s) imported.
+echo 	%count% workshop-id(s) imported
 
 ::	Start the dedicated server
 echo. && echo. && echo Script will now attempt start the server. && echo.
 pause
+
 %SteamCMD_Location%"\steamcmd.exe" +login anonymous +app_update 343050 +quit
 cd /D %binDir%
 start dontstarve_dedicated_server_nullrenderer -console -cluster MyDediServer -shard Master
